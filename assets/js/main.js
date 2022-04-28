bf1ba71926c1974acedafa8c892b4f7690e3e7d3
werthmannsupercode <werthmannsupercode@gmail.com>
2022-04-28 08:56:43 +0200

    media query 576px

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('it works')
+let sideMenu = document.getElementById('side_menu')
+
+function slideIn() {
+    sideMenu.style.right = 0;
+    sideMenu.style.left = '60%';
+}
+
+function slideOut() {
+    sideMenu.style.right = '-100%';
+    sideMenu.style.left = '100%';
+}
